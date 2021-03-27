d181cdf19ade1e69089f6b44c427cdebec98defb
c17an <kimchanmin1@gmail.com>
2021-03-27 18:10:57 +0900

    fix: 메인에 타자기 이펙트 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,6 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/data`,
-        plugins: ["gatsby-transformer-json"],
         name: `data`,
       },
     },

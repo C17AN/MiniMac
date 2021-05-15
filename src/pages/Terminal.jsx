b4fbaa58e586250eebff00732676f2906dbc8ccd
c17an <kimchanmin1@gmail.com>
2021-05-16 00:25:38 +0900

    fix: 터미널이 현재 시간을 출력하도록 수정

--- src/pages/Terminal.jsx
+++ src/pages/Terminal.jsx
@@ -1,15 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import PostTemplate from "../components/PostTemplate/PostTemplate"
 import "../styles/pages/Terminal.scss"
 
 function Terminal({ data, location }) {
+  const [date, setDate] = useState(new Date())
+  useEffect(() => {
+    setDate(new Date())
+  }, [])
   return (
     <Layout location={location}>
       <PostTemplate classAlias="Terminal">
         <div className="fakeScreen">
           <div className="line-container">
-            <p className="login">Last login: Sat May 15 15:02:02</p>
+            <p className="login">
+              Last login: {date.toDateString()}{" "}
+              {date.getHours() > 10
+                ? date.getHours()
+                : "0" + String(date.getHours())}
+              :
+              {date.getMinutes() > 10
+                ? date.getMinutes()
+                : "0" + String(date.getMinutes())}
+              :
+              {date.getSeconds() > 10
+                ? date.getSeconds()
+                : "0" + String(date.getSeconds())}
+            </p>
             <p className="git-add">$ git add .</p>
             <p className="git-commit">
               $ git commit -m "feat: 안녕하세요, 찬민입니다!"

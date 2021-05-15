55401f9a0fe823514e7bc202002e0ff8212b1fd5
c17an <kimchanmin1@gmail.com>
2021-05-15 16:17:43 +0900

    터미널 애니메이션 추가

--- src/pages/Terminal.jsx
+++ src/pages/Terminal.jsx
@@ -9,9 +9,14 @@ function Terminal({ data, location }) {
       <PostTemplate classAlias="Terminal">
         <div className="fakeScreen">
           <div className="line-container">
-            <p className="line1">
+            <p className="login">Last login: Sat May 15 15:02:02</p>
+            <p className="git-add">$ git add .</p>
+            <p className="git-commit">
               $ git commit -m "feat: 안녕하세요, 찬민입니다!"
-              <span className="cursor1">_</span>
+            </p>
+            <p className="commit-message">
+              [master 0f938c0] feat: 안녕하세요, 찬민입니다!
+              <span className="cursor4"></span>
             </p>
             <p className="line4">
               {">"}
